0160721fb1a2148fd3b26a67f6d8c19ace0afd97
TaherMustansir1929 <eren.jeager1929101@gmail.com>
2025-07-21 23:33:44 +0500

    06.4: Implement Accounts Edit Functionality

---
+++ features/accounts/api/use-edit-account.ts
@@ -0,0 +1,44 @@
+import { InferRequestType, InferResponseType } from "hono";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+
+import { client } from "@/lib/hono";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.accounts)[":id"]["$patch"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.accounts)[":id"]["$patch"]
+>["json"];
+
+export const useEditAccount = (id?: string) => {
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (json) => {
+      const response = await client.api.accounts[":id"]["$patch"]({
+        json,
+        param: { id },
+      });
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`HTTP ${response.status}: ${errorText}`);
+      }
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Updated account successfully");
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+      queryClient.invalidateQueries({ queryKey: ["one-account", { id }] });
+      // TODO: Invalidate summary and transactions
+    },
+    onError: (err) => {
+      toast.error("Failed to update account");
+      console.error("Error updating account:", err);
+    },
+  });
+
+  return mutation;
+};
